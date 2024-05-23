814549aaae097a4eae4de30fdb88e21753e182c5
Justin-N-Ho <152570351+Justin-N-Ho@users.noreply.github.com>
2024-05-23 11:59:51 -0700

    control update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,22 +27,33 @@ public class Robot extends TimedRobot {
    */
 
   int i_left_leader = 1, i_left_follower_1 = 2, i_left_follower_2 = 3, i_right_leader = 14, i_right_follower_1 = 15, i_right_follower_2 = 16,
-      i_shooter = 7, i_shooter_2 = 8, i_shooter_slave = 9, i_shooter_2_slave = 10,
-      i_shooter_extension = 11, 
-      i_intake = 12, i_intake_slave = 13;
+      i_left_shooter = 7, i_right_shooter = 8, 
+      i_intake_climb = 9, 
+      i_shooter_extension = 10, 
+      i_hopper_left = 11, i_hopper_right = 12,
+      i_intake = 13;
 
   TalonFX left_leader = new TalonFX(i_left_leader);
   TalonFX left_follower_1 = new TalonFX(i_left_follower_1);
   TalonFX left_follower_2 = new TalonFX(i_left_follower_2);
+
   TalonFX right_leader = new TalonFX(i_right_leader);
   TalonFX right_follower_1 = new TalonFX(i_right_follower_1);
   TalonFX right_follower_2 = new TalonFX(i_right_follower_2);
 
-  TalonFX shooter = new TalonFX(i_shooter);
-  TalonFX shooter2 = new TalonFX(i_shooter_2);
-  TalonFX shooter_slave = new TalonFX(i_shooter_slave);
-  TalonFX shooter_2_slave = new TalonFX(i_shooter_2_slave);
+  TalonFX left_shooter = new TalonFX(i_left_shooter);
+  TalonFX right_shooter = new TalonFX(i_right_shooter);
+
+  TalonFX intake_climb = new TalonFX(i_intake_climb);
+
   TalonFX shooter_extension = new TalonFX(i_shooter_extension);
+
+  TalonFX hopper_left = new TalonFX(i_hopper_left);
+  TalonFX hopper_right = new TalonFX(i_hopper_right);
+
+  TalonFX intake = new TalonFX(i_intake);
+
+
 
   PS4Controller driver = new PS4Controller(0);
   PS4Controller operator = new PS4Controller(1);
@@ -56,7 +67,9 @@ public class Robot extends TimedRobot {
   private Compressor compressor;
 
   private boolean compressorIsRunning;
+
   private boolean pneumaticOn;
+  private boolean intakeOn;
 
   @Override
   public void robotInit() {
@@ -75,6 +88,7 @@ public class Robot extends TimedRobot {
     compressor = new Compressor(PneumaticsModuleType.CTREPCM); 
 
     pneumaticOn = false;
+    intakeOn = false;
   }
 
   public void pneumaticHandler(boolean position){
@@ -85,7 +99,7 @@ public class Robot extends TimedRobot {
     rightSolenoid_retract.set(!position);
   }
 
-  public void compressorHandler(boolean compOn){
+  /*public void compressorHandler(boolean compOn){
     if (compOn == true){
       compressor.enableHybrid(0.5, 1); //TODO figure out what this is and if it's what we need
       compressorIsRunning = true;
@@ -98,7 +112,7 @@ public class Robot extends TimedRobot {
     }
 
     System.out.println("Compressor pressure is low: " + compressor.getPressureSwitchValue());
-  }
+  } *///probably not necessary
 
   @Override
   public void autonomousInit() {
@@ -129,13 +143,32 @@ public class Robot extends TimedRobot {
     left_leader.set(left);
     right_leader.set(right);
 
-    if (operator.getCrossButton()){
+    pneumaticHandler(pneumaticOn);
+
+    if (operator.getCrossButtonPressed()){
       if (pneumaticOn == false){
         pneumaticOn = true;
       } else {
         pneumaticOn = false;
       }
     }
-    pneumaticHandler(pneumaticOn);
+    if (driver.getCircleButtonPressed()){
+      if (intakeOn == false){
+        intake.set(1);
+        intake_climb.set(1);
+        intakeOn = true;
+      } else if (intakeOn == true){
+        intake.set(0);
+        intake_climb.set(0);
+        intakeOn = false;
+      }
+    }
+    if (driver.getR2Button){
+      left_shooter.set(1);
+      right_shooter.set(1);
+    } else {
+      left_shooter.set(0);
+      right_shooter.set(0);
+    }
   }
 }
